ef50d49f931ed707193e4a6098070e5a479e30fb
DEV <dev@NYMACDEVS1.local>
2019-09-13 12:00:35 -0400

    starting mocking up prepare script to automate... not really that necessary since I will likely only have to do this once or twice

---
+++ prepare.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#echo install yarn
+#npm install -g yarn
+
+#echo build lib
+#yarn run build
+
+#build will still be in dist/ so how to
+
+
+#echo push tags
+#git push --follow-tags origin master
+
+#echo push to npm
+#cp README.md ./dist/ng-select/
+#cd ./dist/ng-select/
+#yarn publish --access=public
+
+
+
+#postinstall with dist
+
+
+#place in client repo to test
+#npm install <ghusername>/<repoName>#branchName
